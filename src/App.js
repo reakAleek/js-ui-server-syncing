46b716c4f559315e3765677363d25274c260969a
Jan Calanog <jan.calanog@iteratec.at>
2018-11-02 11:50:15 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ActionBar from "./components/ActionBar";
 
 const App = () => (
     <React.Fragment>
-        <section className="hero is-info">
+        <section className="hero is-primary is-bold">
             <div className="hero-body">
                 <div className="container has-text-centered">
                     <h1 className="title">
--- src/components/ActionBar.js
+++ src/components/ActionBar.js
@@ -6,10 +6,10 @@ import type {AppState} from "../+store/model";
 const ActionBar = ({ addPerson, fetch, isSyncing }) => (
     <div className="field is-grouped">
         <p className="control" style={{ width: '50%'}}>
-            <button className="button is-fullwidth is-success" onClick={() => addPerson()}>Add Person</button>
+            <button className="button is-fullwidth is-outlined is-success" onClick={() => addPerson()}>Add Person</button>
         </p>
         <p className="control" style={{ width: '50%'}}>
-            <button className={`button is-fullwidth is-light ${ isSyncing ? '' : '' } `}
+            <button className={`button is-fullwidth is-info is-outlined ${ isSyncing ? '' : '' } `}
                     disabled={isSyncing}
                     onClick={() => !isSyncing && fetch() }>
                 {isSyncing ? 'Syncing...' : 'Refresh' }
