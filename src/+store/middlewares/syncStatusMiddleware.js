9615c55b75e01aa04d27a926c4c7e05b8433b910
Jan Calanog <jan.calanog@iteratec.at>
2018-11-04 05:42:15 +0100

    adjusted syncing status timings

--- src/+store/middlewares/syncStatusMiddleware.js
+++ src/+store/middlewares/syncStatusMiddleware.js
@@ -1,13 +1,20 @@
 // @flow
 import {Action, Store} from "redux";
 import {Subject} from "rxjs";
-import {FETCH_PERSONS, FETCH_PERSONS_SUCCESS, SAVE_PERSON, SAVE_PERSON_SUCCESS, setSyncStatusAction} from "../actions";
+import {
+    DELETE_PERSON, DELETE_PERSON_SUCCESS,
+    FETCH_PERSONS,
+    FETCH_PERSONS_SUCCESS, PATCH_PERSON, PATCH_PERSON_SUCCESS,
+    SAVE_PERSON,
+    SAVE_PERSON_SUCCESS,
+    setSyncStatusAction, UPDATE_PERSON
+} from "../actions";
 import {debounceTime} from "rxjs/operators";
 
 const syncStatusMiddleware = (store: Store) => {
 
     const syncStatus = new Subject<boolean>();
-    syncStatus.asObservable().pipe(debounceTime(750)).subscribe(isSyncing => {
+    syncStatus.asObservable().pipe(debounceTime(1500)).subscribe(isSyncing => {
         store.dispatch(setSyncStatusAction(isSyncing));
     });
 
@@ -17,12 +24,16 @@ const syncStatusMiddleware = (store: Store) => {
 
             case FETCH_PERSONS:
             case SAVE_PERSON:
+            case PATCH_PERSON:
+            case DELETE_PERSON:
                 store.dispatch(setSyncStatusAction(true));
                 syncStatus.next(true);
                 break;
 
             case FETCH_PERSONS_SUCCESS:
             case SAVE_PERSON_SUCCESS:
+            case PATCH_PERSON_SUCCESS:
+            case DELETE_PERSON_SUCCESS:
                 syncStatus.next(false);
                 break;
 
